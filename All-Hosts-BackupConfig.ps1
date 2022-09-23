a3f96afd225d72407d09e7712aa717100fa6b08e
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-09-23 10:26:57 -0400

    minor revisions

--- All-Hosts-BackupConfig.ps1
+++ All-Hosts-BackupConfig.ps1
@@ -2,16 +2,22 @@
 Remove-Variable * -ErrorAction SilentlyContinue
 
 # The beginning of each script I always include how to create the credential file and how to import it for each script I use.
+
 # Save vCenter credentials - Only needs to be ran once to create .cred file.
 # $credentials = Get-Credential
-# $credentials | Export-Clixml -path <DriveLetter>:\scripts\<localrootpass>.cred
-$credentials = import-clixml -path <DriveLetter>:\scripts\<localrootpass>.cred
+
+# Export cred file
+# $credentials | Export-Clixml -path <DriveLetter>:\scripts\vcsa_admin.cred
+
+# Import cred file
+$credentials = import-clixml -path <DriveLetter>:\scripts\vcsa_admin.cred
 
 #region Starter Vars () - Generic Declarations for this example 
 $vc = "vcenter.fdqn.whatever"
+#endregion
 
 # Connect to vCenter with saved creds
 connect-viserver -Server $vc -Credential $credentials
 
 # Get ESXi host configuration
-Get-VMhost | Get-VMHostFirmware -BackupConfiguration -DestinationPath <DriveLetter>:\backup\Hosts
+Get-VMhost | Get-VMHostFirmware -BackupConfiguration -DestinationPath <DriveLetter>:\backup\hosts
--- Restore-Host-Config.ps1
+++ Restore-Host-Config.ps1
@@ -1,15 +1,21 @@
 # Clear All Variables
 Remove-Variable * -ErrorAction SilentlyContinue
 
-# Script to power off VMs on a dedicated host without vCenter access
 # Save vCenter credentials - Only needs to be ran once to create .cred file.
 # $credential = Get-Credential
-# $credential | Export-Clixml -path <DriveLetter>:\scripts\localroostpass.cred
-# Import cred file
-$credential = import-clixml -path <DriveLetter>:\scripts\localroostpass.cred
-$vmhost = "esxhost1.fqdn.wahtever"
-$restoreConfig = "<DriveLetter>:\backup\hosts\configBundle-esxhost1.fqdn.wahtever.tgz"
 
+# Export cred file
+# $credential | Export-Clixml -path <DriveLetter>:\scripts\esxi_root.cred
+
+# Import cred file
+$credential = import-clixml -path <DriveLetter>:\scripts\esxi_root.cred
+
+#region Started Vars () - Generic Declarations for this example
+$vmhost = "esxhost1.fqdn.wahtever"
+$restoreConfig = "<DriveLetter>:\backup\hosts\configBundle-$($vmhost).tgz"
+#endregion
+
+# Ignore Invalid Certificate warning
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
 
 # Connect to Host with saved creds
