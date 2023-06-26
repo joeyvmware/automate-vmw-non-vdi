f740dceb97319c733272db1a40fea800c8fd782a
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2023-06-25 21:14:36 -0400

    (*.ps1) rewrite for better automation

--- All-Hosts-BackupConfig.ps1
+++ All-Hosts-BackupConfig.ps1
@@ -1,23 +1,39 @@
-# Clear All Variables
-Remove-Variable * -ErrorAction SilentlyContinue
+$existingVariables = Get-Variable
 
-# The beginning of each script I always include how to create the credential file and how to import it for each script I use.
+try {
+    #region Starter Vars () - Generic Declarations for this example
+    # Save vCenter credentials - Only needs to be ran once to create .cred file.
+    $credPath = Read-Host -Prompt "Enter the path to save the vCenter credential file (e.g., C:\scripts\vcsa_admin.cred)"
 
-# Save vCenter credentials - Only needs to be ran once to create .cred file.
-# $credentials = Get-Credential
+    # Create the directory if it doesn't exist
+    if (-not (Test-Path (Split-Path -Path $credPath))) {
+        New-Item -ItemType Directory -Path (Split-Path -Path $credPath) | Out-Null
+    }
 
-# Export cred file
-# $credentials | Export-Clixml -path <DriveLetter>:\scripts\vcsa_admin.cred
+    $credentials = Get-Credential
+    $credentials | Export-Clixml -Path $credPath
 
-# Import cred file
-$credentials = import-clixml -path <DriveLetter>:\scripts\vcsa_admin.cred
+    $vCenter = Read-Host -Prompt "Enter the FQDN of your vCenter Server (e.g., vcenter.fqdn.whatever)"
+    #endregion
 
-#region Starter Vars () - Generic Declarations for this example 
-$vc = "vcenter.fdqn.whatever"
-#endregion
+    # Ignore Invalid Certificate warning
+    Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false | Out-Null
 
-# Connect to vCenter with saved creds
-connect-viserver -Server $vc -Credential $credentials
+    # Connect to vCenter with saved creds
+    Connect-VIServer -Server $vCenter -Credential $credentials
 
-# Get ESXi host configuration
-Get-VMhost | Get-VMHostFirmware -BackupConfiguration -DestinationPath <DriveLetter>:\backup\hosts
+    # Get ESXi host configuration
+    $configPath = Read-Host -Prompt "Enter the path to save host config export file(s) (e.g., C:\backup\hosts\config)"
+
+    # Create the directory if it doesn't exist
+    if (-not (Test-Path $configPath)) {
+        New-Item -ItemType Directory -Path $configPath | Out-Null
+    }
+
+    Get-VMhost | Get-VMHostFirmware -BackupConfiguration -DestinationPath $configPath
+
+    Write-Host "`nBackup task completed successfully." -ForegroundColor Green
+} finally {
+    # Remove any new variables
+    Get-Variable | Where-Object Name -NotIn $existingVariables.Name | Remove-Variable -ErrorAction SilentlyContinue
+}
